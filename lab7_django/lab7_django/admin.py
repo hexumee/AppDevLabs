503de340f207e4c52fd417eeea17b2d7b710b377
Dmitriy Nuzhdov <hexumee@gmail.com>
2023-02-14 14:54:02 +0400

    Moved to corresponding branch

--- lab7_django/lab7_django/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Users, Likes, Rating, Post, Tags
-
-admin.site.register(Users)
-admin.site.register(Rating)
-admin.site.register(Likes)
-admin.site.register(Post)
-admin.site.register(Tags)
